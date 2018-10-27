f149675288141a34ea87f2c7be208aa05986a399
AmanpreetBhasin <amanpreetsingh8855@gmail.com>
2018-10-27 22:40:36 +0530

    Update About Us page

--- aboutUs.php
+++ aboutUs.php
@@ -1,6 +1,3 @@
-<?php 
-ob_start();
-?>
 <!DOCTYPE HTML>
 <!--
 	Intensify by TEMPLATED
@@ -9,13 +6,12 @@ ob_start();
 -->
 <html>
 	<head>
-		<title>Testify. Hone your knowledge.</title>
+		<title>Generic - Intensify by TEMPLATED</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		<link rel="stylesheet" href="assets/css/main.css" />
-		<link rel="stylesheet" href="assets/css1/default.css"/>
 	</head>
-	<body>
+	<body class="subpage">
 
 		<!-- Header -->
 			<header id="header">
@@ -62,4 +58,44 @@ ob_start();
                     ?>
 				</ul>
 			</nav>
-	
+
+		<!-- Main -->
+			<section id="main" class="wrapper">
+				<div class="inner">
+					<header class="align-center">
+						<h1>About Us</h1>
+						<p>An online Examination portal to practice questions and hone your knowledge</p>
+					</header>
+					<!--<div class="image fit">
+						<img src="images/pic05.jpg" alt="" />
+					</div>-->
+					<p>Testify is an online examination question practicing portal that helps one to practice questions on a subject of one's own choice. One can even select the number of questions one wants to attempt. </p>
+					<p>Instant view of result post exam is available, where every question has one mark and carries no negative marking.</p>
+					<p>All the previous exam records can be accesed by the person post login in a graphical and tabular manner. Performance graph of last 10 exams can also be viewed for every subject.</p>
+				</div>
+			</section>
+
+		<!-- Footer -->
+			<footer id="footer">
+				<div class="inner">
+					<h2>Get In Touch</h2>
+					<ul class="actions">
+						<li><span class="icon fa-phone"></span> <a href="#">(000) 000-0000</a></li>
+						<li><span class="icon fa-envelope"></span> <a href="#">information@untitled.tld</a></li>
+						<li><span class="icon fa-map-marker"></span> 123 Somewhere Road, Nashville, TN 00000</li>
+					</ul>
+				</div>
+				<div class="copyright">
+					&copy; Untitled. Design <a href="https://templated.co">TEMPLATED</a>. Images <a href="https://unsplash.com">Unsplash</a>.
+				</div>
+			</footer>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
